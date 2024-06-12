c98b6d9eb8c491378ba96972ca1bd0796a22afb5
Vented5 <venteddiagram@gmail.com>
2024-06-11 22:45:49 -0600

    Added prisma pacckages, .env changes, refer to the backend team to connect with local db

--- hrm.js
+++ hrm.js
@@ -9,3 +9,25 @@ hrm.get('/', (req, res) => {
 hrm.listen(port, () => {
     console.log(`Hrm listening on port ${port}`);
 });
+
+/*  ------    MYSQL RAW CONNECTION ----------------
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'H4mb0rgue50',
+    database: 'test'
+});
+
+function connectDb(){
+    connection.connect();    
+    console.log("Ahueso tenemos conexion");
+    connection.query('SHOW TABLES', function (err, rows, fields){
+        if(err) throw err;
+        console.log('The solution is: ', rows[0]);
+    });
+    connection.end();
+}
+
+connectDb();
+*/
