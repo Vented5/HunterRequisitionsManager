cf725b54d6717f0efd408762367e134b4ec249fb
Vented5 <venteddiagram@gmail.com>
2024-07-01 16:56:53 -0500

    Fix Zero Cors enabled for localhost on requisitions routes.

--- hrm.js
+++ hrm.js
@@ -1,13 +1,19 @@
 const express = require("express")
+const cors = require('cors')
 const morgan = require('morgan')
 
 const hrm = express();
 const port = 3010;
 
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionSuccessStatus: 200
+}
+
 //routes
 hrm.use(morgan('short')) //shows al request in console
 hrm.use('/users', require('./routes/user-routes'))
-hrm.use('/requisitions', require('./routes/requisitions-routes')) 
+hrm.use('/requisitions', cors(corsOptions), require('./routes/requisitions-routes')) 
 
 hrm.listen(port, () => {
     console.log(`Hrm listening on port ${port}`);
